824c96a8ef48b2c3da53903ad990f9f3c345ae5e
HeimirDavid <heimir.97@gmail.com>
2020-03-05 20:19:48 +0000

    first test with template inheritance, routing and env variable works

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+from os import path
+if path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
 
+MONGO_URI = os.environ.get("MONGO_URI")
+MONGODB_NAME = os.environ.get("MONGODB_NAME")
+
+
+app.config["MONGODB_NAME"] = MONGODB_NAME
+app.config["MONGO_URI"] = MONGO_URI
+
+#create new instance of PyMongo
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return 'Hello World, again again'
+@app.route('/view_recipes')
+def view_recipes():
+    return render_template("recipes.html", recipes=mongo.db.recipe.find())
 
 
 if __name__ == '__main__':
