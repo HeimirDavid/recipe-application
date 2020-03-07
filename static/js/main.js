12c427a25ae0b3b2607a78790a60279785490eaa
HeimirDavid <heimir.97@gmail.com>
2020-03-07 14:32:54 +0000

    Functionality to allow user to add and remove ingredients field from the add-recipe form has been added

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,20 @@
+$(document).ready(function() { 
+
+
+    //Allows user to add more ingredient fields
+    $('#add_ing_field').click(function() {
+        $('<input placeholder="1tbsp avocado oil" name="ingredients" type="text" class="validate"></input>').insertBefore(this);
+    });
+    
+    //Allows user to remove last ingredient field
+    $('#remove_ing_field').click(function() {
+        $('#ingredients_list input').last().remove();
+    }); 
+
+
+});
+
+
 // Code for the hamburger menu, comes from this tutorial: https://www.youtube.com/watch?v=dIyVTjJAkLw
 const menuBtn = document.querySelector('.menu-button');
 let menuOpen = false;
@@ -10,6 +27,11 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 })
-
-
-
+/*
+function addIngredientField() {
+    const addIngBtn = document.getElementById('add_ing_field');
+    const newField = document.createElement('<input placeholder="1tbsp avocado oil" name="ingredients" type="text" class="validate"></input>');
+    addIngBtn.addEventListener('click', () => {
+        addIngBtn.parentNode.insertBefore(newField, addIngBtn);
+    });
+}*/
